08e3d1e2cac134f8605c2b4b826585765c62b004
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-11 07:57:24 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -5,10 +5,14 @@ def second_challenge
       :matriarch => {}.
       :hero => {},
       :hero_friends => []
+    },
     :capulet => {
-      
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {}.
+      :hero => {},
+      :hero_friends => []
+    }
+}
 
   
 end
